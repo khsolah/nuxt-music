04c21bd346812fd147461063a048cc0de7f089cf
khsolah <khsolah@gmail.com>
2021-07-29 13:24:01 +0800

    feat(type): define strong type for vuex

---
+++ store/index.ts
@@ -0,0 +1,26 @@
+import {
+  Store as VuexStore,
+  CommitOptions,
+  DispatchOptions
+} from 'vuex/types/index'
+import { Actions } from './actions'
+import { Getters } from './getters'
+import { Mutations } from './mutations'
+import { State } from './state'
+
+export interface Store
+  extends Omit<VuexStore<State>, 'commit' | 'dispatch' | 'getters'> {
+  commit<K extends keyof Mutations, P extends Parameters<Mutations[K]>[1]>(
+    key: K,
+    payload: P,
+    options?: CommitOptions
+  ): ReturnType<Mutations[K]>
+  dispatch<K extends keyof Actions, P extends Parameters<Actions[K]>[1]>(
+    key: K,
+    payload: P,
+    options?: DispatchOptions
+  ): ReturnType<Actions[K]>
+  getters: {
+    [K in keyof Getters]: ReturnType<Getters[K]>
+  }
+}
