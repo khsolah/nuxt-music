5e7994c46c49a98cf1ed113fd4c3dd0b98f7a1d3
khsolah <khsolah@gmail.com>
2021-07-31 21:53:15 +0800

    feat(layout): default layout

---
+++ layouts/default.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <Header />
+    <Nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Default'
+})
+</script>
+
+<style lang="css">
+html {
+  font-size: 16px;
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+body {
+  background: #181a1b;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
