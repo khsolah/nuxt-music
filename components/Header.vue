b2e79f45e6d5e41345d0c0e5f5b04fd9e07a8ca9
khsolah <khsolah@gmail.com>
2021-07-31 21:32:44 +0800

    feat(component): header

---
+++ components/Header.vue
@@ -0,0 +1,177 @@
+<template>
+  <header
+    class="
+      flex
+      h-20
+      w-full
+      top-0
+      right-0
+      left-0
+      z-99
+      fixed
+      items-center
+      justify-between
+      xl:h-16
+      px-4
+    "
+  >
+    <NuxtLogo />
+
+    <nav class="flex w-full right-0 absolute justify-center md:py-2">
+      <ul v-if="!searching" class="flex list-none pl-0">
+        <li v-for="item in navData" :key="item.title" class="lg:mx-6">
+          <nuxt-link
+            :to="item.to"
+            class="
+              flex
+              font-medium
+              h-full
+              text-xl text-white
+              leading-snug
+              opacity-50
+              no-underline
+              items-center
+              hover:opacity-100
+            "
+          >
+            <span class="-lg:hidden">{{ item.title }}</span>
+            <Icon
+              :name="item.icon"
+              class="
+                fill-white
+                -md:hidden
+                md:block
+                lg:hidden
+                md:w-6 md:h-6 md:mx-4 md:my-3
+              "
+            />
+          </nuxt-link>
+        </li>
+      </ul>
+      <div
+        class="
+          border-solid border-transparent
+          cursor-pointer
+          flex
+          h-full
+          border-1
+          relative
+          items-center
+          -md:hidden
+        "
+        :class="{ 'bg-[#212121] !border-[#333]': searching }"
+        @click.stop=""
+      >
+        <Icon
+          :name="searching ? 'arrow' : 'magnify'"
+          class="my-3 fill-white mx-4 md:w-6 md:h-6"
+          @click.native="searching = !searching"
+        />
+        <span
+          v-show="!searching"
+          class="font-medium text-xl text-white leading-snug -lg:hidden"
+          @click="searching = true"
+          >搜尋</span
+        >
+        <input
+          v-show="searching"
+          v-model="search"
+          type="text"
+          class="
+            bg-transparent
+            border-none
+            h-full
+            outline-none
+            text-white
+            w-full
+            lg:text-xl lg:leading-snug lg:w-180 lg:pr-14
+            xl:w-215
+          "
+          placeholder="搜尋"
+          @keydown.esc="searching = false"
+        />
+        <Icon
+          v-show="search.length > 0"
+          name="close"
+          class="
+            fill-white
+            top-0
+            right-0
+            z-1
+            absolute
+            lg:w-6 lg:h-6 lg:mx-4 lg:my-3
+          "
+          @click.native="search = ''"
+        />
+      </div>
+    </nav>
+
+    <a
+      href="/auth"
+      class="
+        bg-white
+        rounded-sm
+        cursor-pointer
+        font-medium
+        text-[#030303]
+        z-99
+        inline-flex
+        items-center
+        no-underline
+        text-sm
+        h-8
+        px-4
+      "
+      >登入</a
+    >
+  </header>
+</template>
+
+<script lang="ts">
+import { Route } from 'vue-router'
+import Vue from 'vue'
+
+interface NavData {
+  title: string
+  icon: string
+  to: Partial<Route>
+}
+
+export default Vue.extend({
+  name: 'Header',
+  data() {
+    return {
+      navData: [
+        {
+          title: '首頁',
+          icon: 'home',
+          to: { name: 'index' }
+        },
+        {
+          title: '探索',
+          icon: 'compass',
+          to: { name: 'explore' }
+        },
+        {
+          title: '媒體庫',
+          icon: 'music-box-multiple',
+          to: { name: 'index' }
+        }
+      ] as NavData[],
+      searching: false,
+      search: ''
+    }
+  },
+  mounted() {
+    window.addEventListener('click', () => {
+      this.searching = false
+    })
+  }
+})
+</script>
+
+<style lang="postcss" scoped>
+.nuxt-link-exact-active {
+  @apply !text-opacity-100;
+}
+</style>
