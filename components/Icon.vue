a74035f8fe4120c85b054668330d2fdaafcc1bc0
khsolah <khsolah@gmail.com>
2021-07-31 21:33:11 +0800

    feat(component): svg icon

---
+++ components/Icon.vue
@@ -0,0 +1,18 @@
+<template>
+  <i class="block">
+    <svg class="w-full h-full">
+      <use :href="`${require('../assets/icons/gen/index.svg')}#${name}`" />
+    </svg>
+  </i>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Icon',
+  props: {
+    name: String
+  }
+})
+</script>
