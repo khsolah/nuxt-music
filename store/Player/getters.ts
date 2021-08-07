b92d5da95752e23ca537962d79817a3f979071f1
khsolah <khsolah@gmail.com>
2021-08-08 06:24:04 +0800

    fix(store): avoid change vuex data outside vuex (youtube player)

--- store/Player/getters.ts
+++ store/Player/getters.ts
@@ -4,7 +4,6 @@ import { RootState } from '../state'
 import { State } from './state'
 
 export enum GetterTypes {
-  GET_PLAYER = 'GET_PLAYER',
   GET_PLAYER_STATUS = 'GET_PLAYER_STATUS',
   GET_PROGRESS = 'GET_PROGRESS',
   GET_CURRENT_TIME = 'GET_CURRENT_TIME',
@@ -12,7 +11,6 @@ export enum GetterTypes {
 }
 
 export interface Getters<S = State> {
-  [GetterTypes.GET_PLAYER]: (state: S) => State['player']
   [GetterTypes.GET_PLAYER_STATUS]: (state: S) => State['playerStatus']
   [GetterTypes.GET_PROGRESS]: (state: S) => State['progress']
   [GetterTypes.GET_CURRENT_TIME]: (state: S) => State['currentTime']
@@ -20,7 +18,6 @@ export interface Getters<S = State> {
 }
 
 export enum PlayerGetterTypes {
-  GET_PLAYER = 'Player/GET_PLAYER',
   GET_PLAYER_STATUS = 'Player/GET_PLAYER_STATUS',
   GET_PROGRESS = 'Player/GET_PROGRESS',
   GET_CURRENT_TIME = 'Player/GET_CURRENT_TIME',
@@ -30,7 +27,6 @@ export enum PlayerGetterTypes {
 export interface PlayerGetters extends Namespaced<Getters, 'Player'> {}
 
 const getters: GetterTree<State, RootState> & Getters = {
-  [GetterTypes.GET_PLAYER]: ({ player }) => player,
   [GetterTypes.GET_PLAYER_STATUS]: ({ playerStatus }) => playerStatus,
   [GetterTypes.GET_PROGRESS]: ({ progress }) => progress,
   [GetterTypes.GET_CURRENT_TIME]: ({ currentTime }) => currentTime,
