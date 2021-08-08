ea2686127b2cdd612a9915ed4bc9b0755d6fb134
khsolah <khsolah@gmail.com>
2021-08-08 15:27:49 +0800

    feat(config): load youtube iframe api script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,14 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    script: [
+      {
+        src: 'https://www.youtube.com/iframe_api',
+        async: true,
+        defer: true
+      }
+    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
