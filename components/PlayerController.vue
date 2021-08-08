b1a4b0d9e3fdd97ce4c6f2c43182076797bceec8
khsolah <khsolah@gmail.com>
2021-08-08 15:30:08 +0800

    feat(component): video controllers

---
+++ components/PlayerController.vue
@@ -0,0 +1,299 @@
+<template>
+  <aside
+    v-if="info"
+    class="
+      bg-black
+      flex
+      w-full
+      right-0
+      bottom-0
+      left-0
+      fixed
+      justify-between
+      items-center
+      lg:h-18
+      -lg:h-16
+    "
+  >
+    <div
+      class="
+        bg-black
+        h-45
+        right-12
+        bottom-22
+        w-80
+        fixed
+        -lg:hidden
+        transition-all
+        duration-500
+      "
+      :style="$route.name === 'watch' ? playerSlotStyle : ''"
+    >
+      <div id="player"></div>
+    </div>
+    <input
+      id="progress"
+      v-model="progress"
+      type="range"
+      name="progress"
+      class="
+        border-none
+        cursor-pointer
+        outline-none
+        h-1
+        w-full
+        transition-all
+        -top-2px
+        transition-300
+        appearance-none
+        absolute
+        overflow-hidden
+      "
+      step="1"
+      min="0"
+      :max="durations.time"
+      :style="`background-image: linear-gradient(to right, #f00 0%, #f00 ${
+        (progress * 100) / durations.time
+      }%, #bdbdbd ${(progress * 100) / durations.time}%)`"
+    />
+    <!-- left controls -->
+    <div class="flex-shrink-0 inline-flex items-center justify-center">
+      <div
+        class="cursor-pointer h-10 ml-2 p-2 w-10"
+        role="button"
+        aria-label="上一首歌"
+        title="上一首歌"
+        @click.prevent=""
+      >
+        <Icon name="skip-previous" class="h-full fill-white w-full" />
+      </div>
+      <div
+        v-show="playerStatus === 'pause'"
+        ref="play"
+        class="cursor-pointer h-12 ml-2 p-2 w-12"
+        role="button"
+        aria-label="播放"
+        title="播放"
+      >
+        <Icon name="play" class="h-full fill-white w-full" />
+      </div>
+      <div
+        v-show="playerStatus === 'play'"
+        ref="pause"
+        class="cursor-pointer h-12 ml-2 p-2 w-12"
+        role="button"
+        aria-label="暫停"
+        title="暫停"
+      >
+        <Icon name="pause" class="h-full fill-white w-full" />
+      </div>
+      <div
+        class="cursor-pointer h-10 ml-2 p-2 w-10"
+        role="button"
+        aria-label="下一首歌"
+        title="下一首歌"
+        @click.prevent=""
+      >
+        <Icon name="skip-next" class="h-full fill-white w-full" />
+      </div>
+      <span class="font-normal mr-4 text-xs ml-2 text-gray-400 -lg:hidden">
+        {{ (currentTime.minutes > 10 ? '' : '0') + currentTime.minutes }}:{{
+          (currentTime.seconds > 10 ? '' : '0') + currentTime.seconds
+        }}&nbsp;/&nbsp;{{ durations.timeString }}
+      </span>
+    </div>
+
+    <!-- middle controls -->
+    <div class="inline-flex items-center justify-center">
+      <img
+        class="h-10 w-auto -lg:hidden"
+        :src="info.snippet.thumbnails.default.url"
+        alt=""
+      />
+
+      <div class="flex flex-col flex-1 mr-2 text-white ml-4">
+        <span class="font-medium line-clamp-1 lg:text-sm 2xl:text-base">{{
+          info ? info.snippet.title : ''
+        }}</span>
+        <span class="text-xs line-clamp-1">{{
+          info ? info.snippet.channelTitle : ''
+        }}</span>
+      </div>
+
+      <div class="flex flex-shrink-0 -lg:hidden">
+        <div
+          class="h-10 mr-2 p-2 w-10"
+          role="button"
+          title="喜歡"
+          aria-label="喜歡"
+        >
+          <Icon
+            name="thumb-up-outline"
+            class="cursor-pointer h-full fill-white w-full"
+          />
+        </div>
+        <div
+          class="h-10 p-2 w-10"
+          role="button"
+          title="不喜歡"
+          aria-label="不喜歡"
+        >
+          <Icon
+            name="thumb-down-outline"
+            class="cursor-pointer h-full fill-white w-full"
+          />
+        </div>
+      </div>
+    </div>
+
+    <!-- right controls -->
+    <div class="mr-1 inline-flex items-center justify-center">
+      <div
+        class="cursor-pointer h-10 p-2 w-10"
+        :aria-label="
+          $route.name === 'watch' ? '關閉播放器頁面' : '開啟播放器頁面'
+        "
+        role="button"
+        @click="togglePlayer"
+      >
+        <Icon
+          name="play"
+          class="
+            h-full
+            fill-white
+            w-full
+            transform
+            transition-transform
+            duration-300
+          "
+          :class="{
+            'rotate-90': $route.name === 'watch',
+            '-rotate-90': $route.name !== 'watch'
+          }"
+        />
+      </div>
+    </div>
+  </aside>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Store } from '~/store'
+
+export default Vue.extend({
+  name: 'PlayerController',
+  data() {
+    return { playerStyle: {} }
+  },
+  async fetch() {
+    if (!this.$route.query.v) return
+
+    await (this.$store as Store).dispatch('Player/FETCH_VIDEO_INFO', {
+      v: this.$route.query.v as string,
+      playlistId: this.$route.query.list as string | null | undefined
+    })
+    ;(this.$store as Store).commit(
+      'Player/SET_DURATIONS',
+      this.info!.contentDetails.duration
+    )
+    ;(this.$store as Store).commit('Player/SET_PROGRESS', 0)
+
+    await this.fetchPlayerQueue()
+  },
+  computed: {
+    info() {
+      return (this.$store as Store).getters['Player/GET_CURRENT_VIDEO_INFO']
+    },
+    currentTime() {
+      return (this.$store as Store).getters['Player/GET_CURRENT_TIME']
+    },
+    durations() {
+      return (this.$store as Store).getters['Player/GET_DURATIONS']
+    },
+    progress: {
+      get() {
+        return (this.$store as Store).getters['Player/GET_PROGRESS']
+      },
+      set(value: number) {
+        return (this.$store as Store).dispatch('Player/SEEK_TO', value)
+      }
+    },
+    playerStatus() {
+      return (this.$store as Store).getters['Player/GET_PLAYER_STATUS']
+    },
+    playlist() {
+      return (this.$store as Store).getters['Player/GET_PLAYLIST']
+    },
+    videos() {
+      return (this.$store as Store).getters['Player/GET_VIDEOS']
+    },
+    playerSlotStyle() {
+      return (this.$store as Store).getters['Player/GET_PLAYER_SLOT_STYLE']
+    }
+  },
+  watch: {
+    async $route() {
+      if (
+        !this.$route.query.v ||
+        (this.info !== null && this.info.id === this.$route.query.v)
+      )
+        return
+
+      await this.$fetch()
+
+      await (this.$store as Store).dispatch(
+        'Player/LOAD_BY_VIDEO_ID',
+        `${this.$route.query.v}`
+      )
+    }
+  },
+  mounted() {
+    // load youtube iframe api
+    // LINK https://developers.google.com/youtube/iframe_api_reference
+    ;(window as any).onYouTubeIframeAPIReady = () =>
+      (this.$store as Store).dispatch(
+        'Player/INIT_PLAYER',
+        `${this.$route.query.v}`
+      )
+
+    this.fetchPlayerQueue()
+  },
+  methods: {
+    async fetchPlayerQueue() {
+      if (!this.$route.query.list) return
+
+      await (this.$store as Store).dispatch(
+        'Player/FETCH_PLAYER_QUEUE',
+        this.$route.query.list as string | undefined | null
+      )
+    },
+    togglePlayer() {
+      if (this.$route.name === 'watch') {
+        this.$router.push({
+          name: 'index'
+        })
+      } else {
+        this.$router.push({
+          name: 'watch',
+          query: {
+            v: this.info!.id,
+            list: this.info!.playlistId
+          }
+        })
+      }
+    }
+  }
+})
+</script>
+
+<style lang="css" scoped>
+input[type='range']::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  position: relative;
+  width: 10px;
+  height: 10px;
+  background: #f22;
+  border-radius: 50%;
+  transition: 0.2s;
+}
+</style>
